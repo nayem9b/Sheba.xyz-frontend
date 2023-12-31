9b3daf48abd9e264ec9b594f8e5c0e612fbcbbe1
Md Razwan Niam <razwanniam1@gmail.com>
2023-12-31 12:40:47 +0600

    category route

--- app/(withlayout)/addcategory/page.tsx
+++ app/(withlayout)/addcategory/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 
 import { usePostCategoryMutation } from "@/redux/api/categoryApi";
-import { Button, message } from "antd";
+import { Button, Upload, message } from "antd";
 import { useRouter } from "next/navigation";
 import { Router } from "next/router";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import type { UploadChangeParam } from "antd/es/upload";
+import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
+import { useState } from "react";
+
+const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file: RcFile) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
 
 const AddCategorypage = () => {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
   const router = useRouter();
   const [postCategory] = usePostCategoryMutation();
 
@@ -54,6 +78,28 @@ const AddCategorypage = () => {
         }
       });
   };
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+    const handleChange: UploadProps["onChange"] = (
+      info: UploadChangeParam<UploadFile>
+    ) => {
+      if (info.file.status === "uploading") {
+        setLoading(true);
+        return;
+      }
+      if (info.file.status === "done") {
+        // Get this url from response in real world.
+        getBase64(info.file.originFileObj as RcFile, (url) => {
+          setLoading(false);
+          setImageUrl(url);
+        });
+      }
+    };
+
   return (
     <div>
       <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
@@ -103,6 +149,21 @@ const AddCategorypage = () => {
           </div>
         </form>
       </div>
+      <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="http://localhost:5000/api/v1/"
+        beforeUpload={beforeUpload}
+        onChange={handleChange}
+      >
+        {imageUrl ? (
+          <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+        ) : (
+          uploadButton
+        )}
+      </Upload>
     </div>
   );
 };
