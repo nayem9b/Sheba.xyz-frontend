a8ae6e99d067fc00917a84c87f2c2a7ad88d3050
nayem9b <razwanniam1@gmail.com>
2024-09-05 08:22:58 +0600

    linter checkup

--- redux/hooks.ts
+++ redux/hooks.ts
@@ -3,7 +3,8 @@ import { AppDispatch, RootState } from "./store";
 import { useEffect, useState } from "react";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = 
+useSelector;
 
 // interface IDebounced {
 //   delay: number;
